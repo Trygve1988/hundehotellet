f2f7f43143812754d2026af690325cee9691148a
Trygve Johannessen <233512@usn.no>
2021-02-17 16:46:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="include/style.css" rel="stylesheet" type="text/css">
     <script src="include/script.js" defer> </script>
 </head>
 <body>
 
-    <p>shiiiiiiiiiiiiiit</p>
+    <!-- ************************** 1) fellesTop ************************** -->
+    <?php visNav(); ?>
+
+    <!-- ************************** 2) main **************************-->
+    <main>
+    </main>
+
+    <!-- ************************** 3) fellesBunn **************************-->
+    <?php visFooter(); ?> 
+    <?php visToppKnapp(); ?> 
 
 </body>
 </html>
+
+
+
+
+
+
+
+
