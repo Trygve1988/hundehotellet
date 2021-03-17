c5fa8c7a0884bb2706de623c686750c5ab18266d
Kingbing35 <even.haaland@hotmail.no>
2021-03-17 17:19:50 +0100

    fikset bug

--- index.php
+++ index.php
@@ -60,43 +60,6 @@
                     </div>
                     <a id="nesteAnmeldelseKnapp" class="anmeldelseBoksKnapp">&#10095;</a>
                 </div>
-      <div class="body">
-        <img  class="background" src="bilder/bakgrund.jpg">
-
-        <div class="front">
-
-          <section>
-            <img src="bilder/hundSlider1.jpg" id="slider">
-            
-            <ul class="navigation">
-              <li onclick="imgSlider('bilder/hundSlider1.jpg')"><img src="bilder/hundSlider1.jpg"></li>
-              <li onclick="imgSlider('bilder/hundSlider2.jpg')"><img src="bilder/hundSlider2.jpg"></li>
-              <li onclick="imgSlider('bilder/hundSlider3.jpg')"><img src="bilder/hundSlider3.jpg"></li>
-            </ul>
-          </section>
-          
-          <div class="text">
-            <p>Velkommen til Bø Hundehotell</p>
-            <p>Norges BESTE Hundehotell for dine firbente venner</p>
-            <p >Åpningstider: Man-Fre 8-18, Lør-Søn: 10-16</p>
-          </div>
-
-          <div class="miniBilde">  
-            <a href="Opphold.html"> <img src="bilder/hunder1.jpg" class="bilder"></a>
-            <a href="Om Oss.html"><img src="bilder/hunder2.jpg" class="bilder"></a>
-            <a href="Pris.php"><img  src="bilder/hunder3.jpg" class="bilder"></a>
-          </div>
-
-          <!-- Trygve anmeldelseslider -->
-          <div id="anmeldelseBoks"> 
-            <a id="tilbakeAnmeldelseKnapp" class="anmeldelseBoksKnapp">&#10094;</a>  
-            <div id="anmeldelseTekstBoks">
-                <p id="anmeldelseTekst"></p>
-            </div>
-            <a id="nesteAnmeldelseKnapp" class="anmeldelseBoksKnapp">&#10095;</a>
-        </div>
-
-
     </main>
 
     <!-- ************************** 3) fellesBunn **************************-->
