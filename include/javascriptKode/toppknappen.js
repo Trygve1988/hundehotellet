dd114d5b3099b4b62e5ec04dea87cd49bbc6f77e
Kristina <233513@usn.no>
2021-03-17 16:49:01 +0100

    Lagt til Footer, CSS og Toppknapp

---
+++ include/javascriptKode/toppknappen.js
@@ -0,0 +1,26 @@
+//https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+//Javascript kode fra w3schools, endret navn på variabler, klasser, funksjoner.
+//Var har blitt gjort om til let (fikk streng beskjed av foreleser på NTNU om om å bare bruke let istenfor var og const).
+
+
+// Henter topp knappen ved hentet elmentet id kalt Knappen
+let minKnapp = document.getElementById("Knappen");
+
+// Når brukeren ruller 20 px fra toppen av dokumentet, viser knappen  
+window.onscroll = function() {scrolleFunksjon()};
+
+// Lager funksjonen som gjør det mulig å skrolle ned på siden
+function scrolleFunksjon() {
+    // Topp knappen dukker opp når brukeren scroller ned
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    minKnapp.style.display = "block";
+} else { /*Toppknapp forsvinner når bruker skroller opp*/ 
+    minKnapp.style.display = "none";
+}
+}
+
+// Når brukeren klikker på knappen, scrolles det til toppen av dokumentet 
+function toppKnappFunksjon() {
+document.body.scrollTop = 0; // Denne koden brukes hvis nettleseren er Safari
+document.documentElement.scrollTop = 0; // Denne koden brukes hvis nettleseren er Chrome, Firefox, Opera og IE.
+}
