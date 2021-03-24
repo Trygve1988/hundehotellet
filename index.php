2432536fdbd8997f6c55cb536ac5a702819a0860
Trygve Johannessen <233512@usn.no>
2021-03-24 17:31:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -56,50 +56,9 @@
     <!-- ************************** 3) fellesBunn **************************-->
     <?php visFooter(); ?>
 
-    <!-- Footer lagt til-->
-    <footer class="main-footer">
-        <div class="venstre">
-            <h1>Kontakinformsjon</h1>
-            <p>Bø Hundehotell</p>
-            <p><strong>Tlf:</strong> 12345678</p>
-            <p><strong>E-post:</strong><a href="bøhundehotell@gmail.com">bøhundehotell@gmail.com</a></p>
-            <p> <strong>Adresse:</strong>Lektorvegen 91 <br> 3802 Bø i Telemark</p>
-        </div>
-
-        <div class="midten sosiale-medier">
-            <h1>Sosiale medier</h1>
-
-            <a href="https://www.instagram.com" target="_blank">
-                <img src="bilder/Logo/facebook.xcf" alt="Instagram Logo" class="instagram-ikon"></a>
-
-            <a href="https://www.facebook.com" target="_blank">
-                <img src="bilder/Logo/facebook.png" alt="Facebook Logo" class="facebook-ikon"></a>
-
-            <a href="https://twitter.com/twitter" target="_blank">
-                <img src="bilder/Logo/twitter.png" alt="Twitter Logo" class="twitter-ikon"></a>
-        </div>
-
-        <!-- Gratis google kart fra https://maps-website.com-->
-        <div class="høyre kart">
-            <h1>Besøk oss</h1>
-            <iframe width="350" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=350&amp;height=200&amp;hl=en&amp;q=Lektorvegen%2091%20B%C3%B8%20i%20Telemark+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-            <a href='https://addmap.net/'>google maps directions embed</a>
-            <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=83957d2396c89fcb76438cfa7afc7c07aeee769a'></script>
-
-        </div>
-
-        <div class="høyre">
-            <h1>Samarbeidspartnere</h1>
-            <p>Royal Canin</p>
-        </div>
-
-    </footer>
-
     <button onclick="toppKnappFunksjon()" id="Knappen" title="Gå til toppen"><i class="fas fa-chevron-up"></i> </button> <!-- gratis Opp ikon fra https://fontawesome.com/icons/chevron-up?style=solid-->
 
     <script src="./include/javascriptKode/toppknappen.js"></script>
-
-
 
     <?php visToppKnapp(); ?>
 
