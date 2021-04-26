c935d8f3256e0a9e36b86bb3fabcb87a2693def3
Kingbing35 <even.haaland@hotmail.no>
2021-04-26 16:03:11 +0200

    Bildeslider som "funker"

--- index.php
+++ index.php
@@ -27,30 +27,34 @@ $dblink = kobleOpp();
         <div class="hvitBakgrunn">
             <img class="bildeBakgrunn" src="bilder/bakgrunn.jpg">
 
-    
-                <div class="Slideshow-container">
+                <!-- Slideshow container -->
+                <div class="slideshow-container">
+
+                    <!-- Full-width images with number and caption text -->
                     <div class="mySlides fade">
-                        <div class="numbertext">1 / 3</div>
-                        <img src="bilder/hundSlider1.jpg" alt="Bilde av hund fra bildeslider">
-                    </div>  
-    
+                        <img src="bilder/hundSlider1.jpg" style="width:100%">
+                    </div>
+
                     <div class="mySlides fade">
-                        <div class="numbertext">2 / 3</div>
-                        <img src="bilder/hundSlider2.jpg" alt="Bilde av hund fra bildeslider">
+                        <img src="bilder/hundSlider2.jpg" style="width:100%">
                     </div>
-    
+
                     <div class="mySlides fade">
-                        <div class="numbertext">3 / 3</div>
-                        <img src="bilder/hundSlider3.jpg" alt="Bilde av hund fra bildeslider">
+                        <img src="bilder/hundSlider3.jpg" style="width:100%">
                     </div>
-    
-                    <div style="text-align: center;">
-                        <span class="dot"></span>
-                        <span class="dot"></span>
-                        <span class="dot"></span>
+
+                    <!-- Next and previous buttons -->  
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>    
                     </div>
-                </div>
-                      
+                    <br>
+
+                    <!-- The dots/circles -->
+                    <div style="text-align:center">
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span>
+                </div>  
 
                 <div class="velkommenText">
                     <p>Velkommen til Bø Hundehotell</p>
