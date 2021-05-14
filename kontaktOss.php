bb52c13beda1b84efec9efffd7ed39d7acd19ce5
Trygve Johannessen <233512@usn.no>
2021-05-14 14:32:49 +0200

    fjernet gammel bildebakgrund fra kontakt oss

--- kontaktOss.php
+++ kontaktOss.php
@@ -30,7 +30,6 @@ $dblink = kobleOpp();
         <!-- 2a kontakt oss -->
         <!--<form method="POST"> -->
         <div class="hvitBakgrunn">
-            <img class="bildeBakgrunn" src="bilder/bakgrunn.jpg">
 
             <!--********************** Kristina ************************** -->
 
