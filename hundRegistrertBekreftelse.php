bd26d64da2070b3ce04088a23fa81fafed795977
Miss-Gunni <78908271+Miss-Gunni@users.noreply.github.com>
2021-05-14 13:24:08 +0200

    Opprettet hundRegistrertBekreftelse.php
    Har opprettet en tilbakemelding på registrering av ny hund

---
+++ hundRegistrertBekreftelse.php
@@ -0,0 +1,66 @@
+<?php
+include_once "include/funksjoner.php";
+session_start();
+$dblink = kobleOpp();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bø Hundehotell</title>
+	<link href="include/style.css" rel="stylesheet" type="text/css">
+	<!--Gratis - Henter opp ikonet fra fontawesome sitt bibliotek-->
+	<script src="https://kit.fontawesome.com/f4f0ae0c65.js" crossorigin="anonymous"></script>
+	<script src="include/script.js" defer> </script>
+</head>
+
+<body>
+
+	<!-- ************************** 1) fellesTop ************************** -->
+	<?php visNav(); ?>
+
+
+	<!-- ************************** 2) main (Gunni) **************************-->
+	<main>
+  
+		<!-- erLoggetInn sjekk -->
+		<?php if (!erLoggetInn()) { header('Location: loggInn.php'); } ?>
+
+        <!-- Hvit bakgrunn -->	
+        <div class="hvitBakgrunn">
+
+            <!-- Bildebakgrunn -->	
+            <img class="bildeBakgrunn" src="bilder/bakgrunn.jpg">
+
+            <!-- Skjema -->		
+            <form class="skjemaBakgrunn">
+            
+                	<!-- Avbryt knapp -->
+				<a href = "index.php">
+					<input class="avbrytKnapp" type="button" value="X">
+				</a>	
+
+                <!-- Overskrift -->
+                <h2>Registrer ny hund</h2>	
+
+                <div>
+                    <p>Hunden er registrert!</p>
+                </div>
+            </form>
+        </div>
+    </main>
+	<!-- Til-toppen-knapp -->
+	<button onclick="toppKnappFunksjon()" id="Knappen" title="Gå til toppen">Top</button>
+
+	<!-- 2g bestillOpphold -->
+	<?php velgHund($dblink); ?> 
+
+	<!-- ************************** 3) fellesBunn **************************-->
+	<?php visFooter(); ?>
+	<?php visToppKnapp(); ?>
+
+</body>
+</html>
