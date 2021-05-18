9f729aa6adc7f772362598602693596ea0776866
Kristina <233513@usn.no>
2021-05-18 13:48:47 +0200

    Bestilling bekreftelse HTML/CSS

--- bestillingBekreftelse.php
+++ bestillingBekreftelse.php
@@ -38,13 +38,38 @@ $dblink = kobleOpp();
 				<!-- Avbryt knapp -->
 				<input class="inputButton avbrytKnapp" type="button" name="avbryt" value="X">
 
-				<!-- Overskrift -->
-				<h2 class="hovedOverskrift">Bekreftelse på bestilling</h2>
-			
-                <p>SETT IN TEKST FOR BEKREFTELSE PÅ OPPHOLD!!</p> 
-				<!-- Tilbake til forsiden- knapp -->
-                <a href = "index.php">   
-                <input class="inputButton hovedKnapp" type="button" name="tilIndex" value="Tilbake til forsiden">
+
+	<!-- ************************ (Kristina) ************************** -->
+				<div class="luft">
+
+					<div class="takkSiden">
+						<div class="module">
+							<!--Module(fontawesome)  -->
+							<!-- gratis Opp ikon fra https://fontawesome.com/icons/check-circle?style=solid -->
+							<i class="fas fa-check-circle fa-2x"></i>
+							<!-- Overskrift -->
+							<h2 class="hovedOverskrift">Bekreftelse på bestilling</h2>
+
+							<p>Din bestilling er nå mottatt!</p>
+							<p>
+								Har du noe spørsmål angående oppholdet kan du ta kontakt med oss enten på tlf 12345678
+								eller <a href="mailto:bohundehotell@outlook.com">bohundehotell@outlook.com </a>
+							</p>
+
+
+
+							<div class="LinkerTilTakkSiden"> <!-- (Kristina) -->
+								<!-- Tilbake til forsiden- knapp (Gunni)-->
+								<a href="index.php">
+									<input class="inputButton hovedKnapp" type="button" name="tilIndex" value="Tilbake til forsiden">
+									<!-- Tilbake til min Side knapp (Kristina)-->
+									<a href="minSide.php">
+										<input class="inputButton hovedKnapp" type="button" name="tilIndex" value="Tilbake til Min side">
+
+							</div>
+						</div>
+					</div>
+				</div>
 								
 			</form>
 		</div>
