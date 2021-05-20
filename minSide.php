deb3551bad2a817048099be0df12e1d79236e5f2
Miss-Gunni <78908271+Miss-Gunni@users.noreply.github.com>
2021-05-20 12:36:35 +0200

    Lagd testkopi av Min side

--- minSide.php
+++ minSide.php
@@ -32,52 +32,9 @@
 	
             <!-- Skjema -->	
             <form class="skjemaBakgrunn" method="POST">
-
-                <!-- InnloggetInfo -->
-                <?php visInnloggetInfo($dblink); ?>
-                <a href="endreBrukerInfo.php">
-                    <input class="litenKnapp" type="button" value="Endre Bruker" name="Endre Bruker">
-                </a>
-                <a href="endrePassord.php"> 
-                    <input class="litenKnapp" type="button" value="Endre Passord" name="Endre Passord">
-                </a>
-                <br><br><br>
-
-                <!-- MineHunder -->
-                <?php visMineHunder($dblink) ?>
-                <a href="registrerHund.php">
-                    <input class="litenKnapp" type="button" value="Registrer Hund" name="Registrer Hund">
-                </a>
-                <?php if (harHund($dblink)) { ?>
-                    <a href="endreHund1.php">
-                        <input class="litenKnapp" type="button" value="Endre Hund" name="Endre Hund">  
-                    </a>
-                <?php } ?>
-                <br><br><br>
-
-                <!-- Bestillinger --> 
-                <?php visMineOpphold($dblink); ?> 
-                <?php if (harOpphold($dblink)) { ?>
-                    <br>
-                    <a href="Avbestill.php">
-                        <input class="litenKnapp" type="button" value="Avbestill" name="Avbestill"> 
-                    </a> 
-                    <a href="skrivAnmeldelse.php">
-                        <input class="litenKnapp" type="button" value="Skriv Anmeldelse" name="Skriv Anmeldelse">
-                    </a>
-                <?php } ?>
-                
-                <!-- Knappepanel --> 
-                <div class="knappePanel">  
-
-                </div>
             </form> 
-        </div>
+        <div> 
     </main>
-
-    <!-- ************************** 3) fellesBunn **************************-->
-    <?php visFooter(); ?> 
-    <?php visToppKnapp(); ?> 
-   
+    <footer></footer>
 </body>
 </html>
