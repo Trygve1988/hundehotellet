42772d223b69600202b76e3dadd533ee9c7f2adc
Trygve Johannessen <233512@usn.no>
2021-05-22 19:06:35 +0200

    fikset font awsome imprt på alle sider

--- endreHund2.php
+++ endreHund2.php
@@ -11,6 +11,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="include/style.css" rel="stylesheet" type="text/css">
+    <!--Gratis - Henter opp ikonet fra fontawesome sitt bibliotek-->
+    <script src="https://kit.fontawesome.com/f4f0ae0c65.js" crossorigin="anonymous"></script>
     <script src="include/script.js" defer> </script>
 </head>
 <body>
