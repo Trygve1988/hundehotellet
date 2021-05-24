bf27eed2d951bf24d265d468fd8b903d420153b6
Miss-Gunni <78908271+Miss-Gunni@users.noreply.github.com>
2021-05-24 21:49:26 +0200

    Rettet opp skrivefeil

--- include/script.js
+++ include/script.js
@@ -440,7 +440,7 @@ function melding(){
     status.innerHTML="Passord er godkjent";
     return true;
   } else{
-    status.innerHTML="Mellom 8-15 tegn, minst et stor bokstav, og  minnst et speseielt tegn.";
+    status.innerHTML="Mellom 8-15 tegn, minst et stor bokstav, og  minst et speseielt tegn.";
     return false;
   } 
 }
