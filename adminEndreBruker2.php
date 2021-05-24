fdf329589820a9be7efe52aaf66e06819e950dc9
Kristina <233513@usn.no>
2021-05-24 18:57:15 +0200

    Endret overskrift til hovedoverskrift, endret tilbake til avbryt på adminEndreBruker2

--- adminEndreBruker2.php
+++ adminEndreBruker2.php
@@ -34,27 +34,27 @@
 
             <!-- Skjema -->	
             <form class="skjemaBakgrunn" method="POST">
-                <h3>Endre Bruker</h3>  
+            <h1 class="hovedOverskrift">Endre bruker</h1>
 
-                <div class="skjemaKolonner">
+                <div class="skjemaKolonner">X
                     <div class="kolonne1">
-                        <label for="epost">epost:</label>
+                        <label for="epost">Epost:</label>
                         <input class="inputTekst" type="text" id="epost" name="epost" value= <?php echo $bruker->getEpost() ?>>
-                        <label for="tlf">tlf:</label>   
+                        <label for="tlf">Tlf:</label>   
                         <input class="inputTekst" type="text" id="tlf" name="tlf" value= <?php echo $bruker->getTlf() ?> >
-                        <label for="adresse">adresse:</label>     
+                        <label for="adresse">Adresse:</label>     
                         <input class="inputTekst" type="text" id="adresse" name="adresse" value= <?php echo $bruker->getAdresse() ?> > 
                     </div>
                     <div class="kolonne2">
-                        <label for="fornavn">fornavn:</label>   
+                        <label for="fornavn">Fornavn:</label>   
                         <input class="inputTekst" type="text" id="fornavn" name="fornavn" value= <?php echo $bruker->getFornavn() ?> >
-                        <label for="etternavn">etternavn:</label>  
+                        <label for="etternavn">Etternavn:</label>  
                         <input class="inputTekst" type="text" id="etternavn" name="etternavn" value= <?php echo $bruker->getEtternavn() ?> >
                     </div>
                 </div> 
 
                 <a href="admin.php">
-                    <input class="litenKnapp" type="button" value="Tilbake">  
+                    <input class="litenKnapp" type="button" value="Avbryt">  
                 <a>
                 <input class="litenKnapp" type="submit" value="Lagre">  
             </div>
