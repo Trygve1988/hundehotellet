15730483c7bb3ba4773d4fa0ca0e1aab49d24e3a
Miss-Gunni <78908271+Miss-Gunni@users.noreply.github.com>
2021-05-25 18:49:59 +0200

    Fikset på passord

--- minSideEndrePassord.php
+++ minSideEndrePassord.php
@@ -41,25 +41,31 @@
 			    <a href = "minSide.php">
 				    <input class="avbrytKnapp" type="button" value="X">
 			    </a>    
-                
+
+                <!--Overskrift -->
                 <h2 class="hovedOverskrift">Endre passord</h2>
 
 				<div class="skjemaKolonner">
-					<div class="soloKolonne">
-                        <label for="gammeltPassord">Gammelt passord:</label>
-                        <input class="inputPassword gjentaPKolonne" type="password" id="gammeltPassord" pattern="^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*_=+-]).{8,15}$" name="gammeltPassord" >
-                        <!-- Vis passord checkbox -->
-							<div class="visPassord">
-								<label for="passordCheckbox">Vis Passord</label>
-								<input class="inputCheckbox" type="checkbox" name="passordCheckbox" onclick="visPassord()">
-							</div>
+					
+                    <div class="soloKolonne">
+                        
+                    <label for="gammeltPassord">Gammelt passord:</label>
+						<input class="inputPassord" type="password" name="gammeltPassord" required 
+						id="passord" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,15}$" onChange="sjekkPassord()" value="" required>
 
-                        <label for="nyttPassord">Nytt passord:</label>   
-                        <input class="inputPassword" type="password" id="nyttPassord" name="nyttPassord" pattern="^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*_=+-]).{8,15}$" required >  
-                       
-                       <!-- Vis passord checkbox -->
+						<!-- Vis passord checkbox -->
 						<div class="visPassord">
-							<label for="passordCheckbox">Vis Passord</label>
+							<label id="visPassordRegisterDeg" for="passordCheckbox">Vis passord</label>
+							<input class="inputCheckbox" type="checkbox" name="passordCheckbox" onclick="visPassord()">
+						</div>
+
+                        <label id="passordRegisterDeg" for="passord">Nytt passord:</label>
+						<input class="inputPassord" type="password" name="passord" 
+						id="passord" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,15}$" onChange="sjekkPassord()" value="" required >
+
+						<!-- Vis passord checkbox -->
+						<div class="visPassord">
+							<label id="visPassordRegisterDeg" for="passordCheckbox">Vis passord</label>
 							<input class="inputCheckbox" type="checkbox" name="passordCheckbox" onclick="visPassord()">
 						</div>
 						
@@ -71,18 +77,20 @@
 
 						<!-- Passord tilbakemelding -->
 						<div class="passordKrav">
-							<p>Passord krav:</p>
+							<p id="pasokravRegisterDeg">Passord krav:</p>
 							<p id="status" melding()></p>
 							<!-- Engelsk tilbakemelding --->
 							<p id="status2" melding2()></p>
 						</div>
-
-                        <div class="gjentaPKolonne">
-                            <label for="bekreftNyttPassord">Bekreft nytt passord:</label>  
-                            <input class="inputPassword" type="password" id="bekreftNyttPassord" name="bekreftNyttPassord" pattern="^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*_=+-]).{8,15}$" required >
-                        </div>
-                    <!-- oppdaterBrukerInfo -->
-                    <?php endrePassord($dblink) ?> 
+						
+						<!-- ************************ (Gunni) ************************** -->
+						<div class="gjentaPKolonne">
+							<label id="gjentaPasoRegisterDeg" for="passordSjekk">Gjenta passord:</label>
+							<input class="inputPassord" type="password" name="passordSjekk" id="gjentaPassord" onChange="sjekkPassord()" required value="123Ab%12">	
+						</div>	
+				
+                        <!-- oppdaterBrukerInfo -->
+                        <?php endrePassord($dblink) ?> 
                     </div>
                 </div> 
 
